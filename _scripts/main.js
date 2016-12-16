178b58d77c5f90841d48a1bdfb9e619c7b3659ba
tony-jones <ad30jone@gmail.com>
2016-12-16 17:54:11 -0500

    adding remaining banner area

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,9 +1,1 @@
 // Your code goes here.
-
-function ready(fn) {
-  if (document.readyState != 'loading'){
-    console.log("hello world");
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-}
