452698dbfd0cee39133192ede154c374b97467e9
tony-jones <ad30jone@gmail.com>
2017-01-11 14:04:21 -0500

    fix service worker urls

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -144,7 +144,7 @@ gulp.task('generate-service-worker-production', function(callback) {
   swPrecache.write(path.join(rootDir, 'sw.js'), {
     staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,json}'],
     stripPrefix: rootDir,
-    replacePrefix: '/faulkner'
+    replacePrefix: '/'
   }, callback);
 });
 
